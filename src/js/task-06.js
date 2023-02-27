9115dd2fcfe62a2117650beffcd4fe5ae2b14878
Anna Kravets <annakravets1906@gmail.com>
2023-02-28 00:37:55 +0200

    Додано виконання ДЗ № 10

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,7 +4,9 @@ inputEl.addEventListener("blur", () => {
   console.log(inputEl.value.length);
   console.log(Number(inputEl.dataset.length));
   console.log(inputEl.value.length === Number(inputEl.dataset.length));
+
   inputEl.classList.add("invalid");
+
   inputEl.value.length === Number(inputEl.dataset.length)
     ? inputEl.classList.replace("invalid", "valid")
     : inputEl.classList.replace("valid", "invalid");
